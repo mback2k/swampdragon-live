cc6976dc1457a98e58494eac05720202988efb73
Marc Hoersken <info@marc-hoersken.de>
2016-03-27 20:42:20 +0200

    Add support for listening on querysets instead of just instances

--- swampdragon_live/tasks.py
+++ swampdragon_live/tasks.py
@@ -20,3 +20,20 @@ def push_new_content_for_instance(instance_type_pk, instance_pk):
             channel = 'swampdragon-live-%s' % cache_key
             value = get_template(template_name).render(new_context)
             publish_data(channel=channel, data=value)
+
+@task(ignore_result=True)
+def push_new_content_for_queryset(queryset_type_pk):
+    try:
+        channel_cache = caches['swampdragon-live']
+    except InvalidCacheBackendError:
+        channel_cache = caches['default']
+
+    cache_key = 'swampdragon-live.type.%d.queryset' % (queryset_type_pk)
+    cache_keys = channel_cache.get(cache_key, [])
+
+    for cache_key in cache_keys:
+        template_name, new_context = channel_cache.get(cache_key, (None, None))
+        if template_name and new_context:
+            channel = 'swampdragon-live-%s' % cache_key
+            value = get_template(template_name).render(new_context)
+            publish_data(channel=channel, data=value)
--- swampdragon_live/templatetags/swampdragon_live.py
+++ swampdragon_live/templatetags/swampdragon_live.py
@@ -3,8 +3,10 @@ from django.core.cache import InvalidCacheBackendError, caches
 from django.contrib.contenttypes.models import ContentType
 from django.template.loader import get_template
 from django.template import Context, Library
+from django.db.models.query import QuerySet
 from django.db.models import Model
 import hashlib
+import pickle
 
 register = Library()
 
@@ -18,6 +20,23 @@ def listen_on_instance(channel_cache, tag_name, template_name, user, new_context
     channel_cache.set(user_cache_key, (template_name, new_context))
 
     cache_key = 'swampdragon-live.type.%d.instance.%d' % (instance_type.pk, instance.pk)
+    cache_keys = channel_cache.get(cache_key, set())
+    cache_keys.add(user_cache_key)
+    channel_cache.set(cache_key, set(cache_keys))
+
+    return user_cache_key
+
+def listen_on_queryset(channel_cache, tag_name, template_name, user, new_context, queryset):
+    queryset_type = ContentType.objects.get_for_model(queryset.model)
+    queryset_dump = pickle.dumps(queryset.query)
+
+    fragment_hash = hashlib.sha1('%s:%s' % (tag_name, template_name)).hexdigest()
+    queryset_hash = hashlib.sha1('%d:%s' % (queryset_type.pk, queryset_dump)).hexdigest()
+    username_hash = hashlib.sha1('%d:%s' % (user.id, user.username)).hexdigest()
+    user_cache_key = '%s-%s-%s' % (fragment_hash, queryset_hash, username_hash)
+    channel_cache.set(user_cache_key, (template_name, new_context))
+
+    cache_key = 'swampdragon-live.type.%d.queryset' % queryset_type.pk
     cache_keys = channel_cache.get(cache_key, set())
     cache_keys.add(user_cache_key)
     channel_cache.set(cache_key, set(cache_keys))
@@ -40,6 +59,10 @@ def include_live(context, tag_name, template_name, **kwargs):
         if isinstance(value, Model):
             user_cache_key = listen_on_instance(channel_cache, tag_name, template_name,
                                                 user, new_context, instance=value)
+            user_cache_keys.append(user_cache_key)
+        elif isinstance(value, QuerySet):
+            user_cache_key = listen_on_queryset(channel_cache, tag_name, template_name,
+                                                user, new_context, queryset=value)
             user_cache_keys.append(user_cache_key)
 
     channels = map(lambda c: 'swampdragon-live-%s' % c, user_cache_keys)
