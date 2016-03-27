c68a30460df712f46f9e9c33834be0225df2c229
Marc Hoersken <info@marc-hoersken.de>
2016-03-27 18:44:00 +0200

    Only allow users to subscribe to their specific template channels

--- swampdragon_live/routers.py
+++ swampdragon_live/routers.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from swampdragon import route_handler
 from swampdragon.route_handler import BaseRouter
+import hashlib
 
 class LiveTemplateRouter(BaseRouter):
     route_name = 'swampdragon-live'
@@ -8,7 +9,12 @@ class LiveTemplateRouter(BaseRouter):
 
     def get_subscription_channels(self, key, **kwargs):
         if key.startswith('swampdragon-live-'):
-            return [key]
+            user = self.connection.get_user()
+            if user:
+                username_hash = hashlib.sha1('%d:%s' % (user.id, user.username)).hexdigest()
+                if username_hash:
+                    if key.split('-')[-1] == username_hash:
+                        return [key]
         return []
 
 route_handler.register(LiveTemplateRouter)
