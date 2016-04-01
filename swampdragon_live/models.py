28775fdaff5285282180923804fedb08e5dd0105
Marc Hoersken <info@marc-hoersken.de>
2016-04-01 21:23:27 +0200

    Fix template not being updated without additional page hit
    Remove dependency on Celery and require django-transaction-hooks

--- swampdragon_live/models.py
+++ swampdragon_live/models.py
@@ -1,26 +1,26 @@
 # -*- coding: utf-8 -*-
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, pre_delete
 from django.contrib.contenttypes.models import ContentType
 from django.dispatch import receiver
 
-from .tasks import push_new_content_for_instance
-from .tasks import push_new_content_for_queryset
+from .pushers import push_new_content_for_instance
+from .pushers import push_new_content_for_queryset
 
 @receiver(post_save)
 def post_save_handler(sender, instance, **kwargs):
     if ContentType.objects.exists():
         instance_type = ContentType.objects.get_for_model(instance.__class__)
 
-        kwargs = {'queryset_type_pk': instance_type.pk}
-        push_new_content_for_queryset.apply_async(countdown=1, kwargs=kwargs)
+        push_new_content_for_queryset(queryset_type_pk=instance_type.pk,
+                                      queryset_pk=instance.pk)
 
-        kwargs = {'instance_type_pk': instance_type.pk, 'instance_pk': instance.pk}
-        push_new_content_for_instance.apply_async(countdown=1, kwargs=kwargs)
+        push_new_content_for_instance(instance_type_pk=instance_type.pk,
+                                      instance_pk=instance.pk)
 
-@receiver(post_delete)
-def post_delete_handler(sender, instance, **kwargs):
+@receiver(pre_delete)
+def pre_delete_handler(sender, instance, **kwargs):
     if ContentType.objects.exists():
         instance_type = ContentType.objects.get_for_model(instance.__class__)
 
-        kwargs = {'queryset_type_pk': instance_type.pk}
-        push_new_content_for_queryset.apply_async(countdown=1, kwargs=kwargs)
+        push_new_content_for_queryset(queryset_type_pk=instance_type.pk,
+                                      queryset_pk=instance.pk)
