6e6809f3f23dd4c7447405c15a213895e0b86eb9
Marc Hoersken <info@marc-hoersken.de>
2016-04-02 12:59:34 +0200

    Update JavaScript to avoid duplicate subscriptions and DOM updates

--- swampdragon_live/static/swampdragon/js/dist/swampdragon.live.js
+++ swampdragon_live/static/swampdragon/js/dist/swampdragon.live.js
@@ -1,23 +1,38 @@
-swampdragon.open(function () {
-  var nl = document.getElementsByClassName('swampdragon-live');
-  for (var i = 0; i < nl.length; i++) {
-    var n = nl[i];
-    for (var j = 0; j < n.classList.length; j++) {
-      var c = n.classList[j];
-      if (c.lastIndexOf('swampdragon-live-', 0) === 0) {
-        swampdragon.subscribe('swampdragon-live', c, {'key': c}, function (context, data) {
-          swampdragon.onChannelMessage(function (channels, message) {
-            for (var k = 0; k < channels.length; k++) {
-              var c = channels[k];
-              var ml = document.getElementsByClassName(c);
-              for (var l = 0; l < ml.length; l++) {
-                var m = ml[l];
-                m.innerHTML = message.data;
-              };
-            };
-          });
-        }, function (context, data) {});
+var SwampDragonLive = function (sd, el) {
+  var sdc = 'swampdragon-live';
+  var sdl = this;
+  this.sd = sd;
+  this.el = el;
+  this.cs = [];
+  this.listen = function (el) {
+    var nl = el.getElementsByClassName(sdc);
+    for (var i = 0; i < nl.length; i++) {
+      var n = nl[i];
+      for (var j = 0; j < n.classList.length; j++) {
+        var c = n.classList[j];
+        if (c.lastIndexOf(sdc+'-', 0) === 0) {
+          if (sdl.cs.indexOf(c) === -1) {
+            sdl.cs.push(c);
+            sdl.sd.subscribe(sdc, c, {'key': c}, null, null);
+          };
+        };
       };
     };
   };
+  this.listener = function (channels, message) {
+    for (var i = 0; i < channels.length; i++) {
+      var c = channels[i];
+      var ml = sdl.el.getElementsByClassName(c);
+      for (var j = 0; j < ml.length; j++) {
+        var m = ml[j];
+        m.innerHTML = message.data;
+        sdl.listen(m);
+      };
+    };
+  };
+  this.sd.onChannelMessage(this.listener);
+};
+swampdragon.open(function () {
+  swampdragon_live = new SwampDragonLive(swampdragon, document);
+  swampdragon_live.listen(document);
 });
