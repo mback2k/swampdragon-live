bce7c3c5de907c5992c97440006fd1ea36712559
Marc Hoersken <info@marc-hoersken.de>
2016-04-03 18:09:57 +0200

    Refresh user-specific cache on subscription

--- swampdragon_live/routers.py
+++ swampdragon_live/routers.py
@@ -3,6 +3,8 @@ from swampdragon import route_handler
 from swampdragon.route_handler import BaseRouter
 import hashlib
 
+from .utils import get_channel_cache
+
 class LiveTemplateRouter(BaseRouter):
     route_name = 'swampdragon-live'
     valid_verbs = ['subscribe', 'unsubscribe']
@@ -14,6 +16,10 @@ class LiveTemplateRouter(BaseRouter):
                 username_hash = hashlib.sha1('%d:%s' % (user.id, user.username)).hexdigest()
                 if username_hash:
                     if key.split('-')[-1] == username_hash:
+                        channel_cache = get_channel_cache()
+                        data = channel_cache.get(key)
+                        if not data is None:
+                            channel_cache.set(key, data)
                         return [key]
         return []
 
